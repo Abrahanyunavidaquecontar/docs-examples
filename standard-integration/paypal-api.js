de6fd7c91476df4d085bffc06d6e9f34ef569fea
Jesus Abrahan Ocona Sequera <131146458+Abrahanyunavidaquecontar@users.noreply.github.com>
2023-06-07 06:07:02 -0600

    Update paypal-api.js

--- standard-integration/paypal-api.js
+++ standard-integration/paypal-api.js
@@ -1,16 +1,16 @@
 import fetch from "node-fetch";
 
-const { CLIENT_ID, APP_SECRET } = process.env;
+const { CLIENT_2K85TGHQ47J8S, APP_EMAZEK3-gHIFl2TxZC2Qll0pFelDAr0Z8GAFVhyHo1DYX794BZmq6C-KQAIVgcAIt6crUJzLygj4Wfnn } = process.env;
 const base = "https://api-m.sandbox.paypal.com";
 
 export async function createOrder() {
-  const accessToken = await generateAccessToken();
+  const accessToken = await generateAccessToken(access_token$sandbox$xqc6g2hgj5yypnq8$1d6a1dd001953dbb068f4925efe79796);
   const url = `${base}/v2/checkout/orders`;
-  const response = await fetch(url, {
+  const response = await fetch(url, {https://unavidaquecontar.com.mx}
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
+      Authorization: `Bearer ${access_token$sandbox$4zyxxff8mps6mpn3$a16c0dbb1dc85d8f00e4b153980d62ee}`,
     },
     body: JSON.stringify({
       intent: "CAPTURE",
